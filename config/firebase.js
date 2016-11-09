432381ecd2fb9bb56012b6cac3e25fbe62e35e52
Anna Konopka <ania@zdwola.pl>
2016-11-09 09:35:47 +0100

    Fixes after rebase

--- config/firebase.js
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_URL = 'https://project-5613440220430148247.firebaseio.com';
-export const BASE_URL = `${FIREBASE_URL}/api/v1`;
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBKP4cOP508h0JLKmjFvzJooO0MqV8l4fU",
-  authDomain: "project-5613440220430148247.firebaseapp.com",
-  databaseURL: "https://project-5613440220430148247.firebaseio.com",
-  storageBucket: "project-5613440220430148247.appspot.com",
-  messagingSenderId: "122242095723"
-};
