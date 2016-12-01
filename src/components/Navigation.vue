4b5ceda9f9e8a0856fb7d1fb1410470be453b163
Anna Konopka <ania@zdwola.pl>
2016-12-01 23:17:42 +0100

    Fixes for navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template v-mdl>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Categories</span>
+    <nav class="mdl-navigation">
+      <span v-for="item in categories" class="mdl-navigation__link" href="#">
+        <router-link tag="a" :to="'/products/' + item.id" >{{ item.title }}</router-link>
+      </span>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    categories: Array,
+  },
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
