68b61922ce4feac8ea974d133f009d7ed6877671
Anna Konopka <ania@zdwola.pl>
2017-01-12 11:30:05 +0100

    Add indication of active tab

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,8 +2,13 @@
   <div class="mdl-layout__drawer">
     <span class="mdl-layout-title">Categories</span>
     <nav class="mdl-navigation">
-      <span v-for="item in categories" class="mdl-navigation__link" href="#">
-        <router-link tag="a" :to="'/products/' + item.id" >{{ item.title }}</router-link>
+      <span v-for="item in categories">
+        <router-link :to="'/products/' + item.id"
+                      active-class="active"
+                      class="mdl-navigation__link"
+                      exact>
+          <a>{{ item.title }}</a>
+        </router-link>
       </span>
     </nav>
   </div>
@@ -17,9 +22,13 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 a {
   text-decoration: none;
   color: black;
+}
+
+.active {
+  background-color: #c0c0c0;
 }
 </style>
