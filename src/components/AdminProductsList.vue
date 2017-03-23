dbd8910751d0b4c9f2d4ad4f78a23856483bded0
Michal Hans <info@michalhans.pl>
2017-03-23 14:28:59 +0100

    Added products list for admin user

--- src/components/AdminProductsList.vue
+++ src/components/AdminProductsList.vue
@@ -1,12 +1,42 @@
 <template>
   <div>
-    Admin Products List
+    <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Product name</th>
+        <th>Category</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products">
+          <td>{{product.id}}</td>
+          <td>{{product.title}}</td>
+          <td>{{product.category}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
+  import { actionTypes as productAction } from '../store/modules/products';
+
   export default {
     name: 'AdminProductsList',
+    computed: mapState({
+      products: state => state.products.products,
+    }),
+    created() {
+      this.$store.dispatch(productAction.GET_PRODUCTS);
+    },
   };
 
 </script>
+
+<style>
+  TABLE {
+    width: 100%;
+  }
+</style>
